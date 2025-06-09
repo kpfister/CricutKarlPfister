fa22df53c67ee1cb8887a1af1572267c0e96ab4d
Karl Pfister <ikarly@MacBookPro.localdomain>
2025-06-09 10:13:10 -0600

    Initial Commit
    Creates project
    Basic folders
    initial viewState

---
+++ CricutKarlPfister/CricutKarlPfister/Core/CricutKarlPfisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricutKarlPfisterApp.swift
+//  CricutKarlPfister
+//
+//  Created by Karl Pfister on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CricutKarlPfisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CricutKarlPfister/CricutKarlPfisterTests/CricutKarlPfisterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CricutKarlPfisterTests.swift
+//  CricutKarlPfisterTests
+//
+//  Created by Karl Pfister on 6/9/25.
+//
+
+import Testing
+@testable import CricutKarlPfister
+
+struct CricutKarlPfisterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
